5779def0f4b7d7bc5582a72eb04cdfbfcd7dd973
Polina Rasskazova <rasskazova.pm@phystech.edu>
2021-12-13 11:49:39 +0300

    add step 8

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import prompt
+from random import randint
+from brain_games.logic import welcome_user, make_check
+from brain_games.games.progression import make_progression
+
+
+def main():
+    exercise = 'What number is missing in the progression?'
+    name = welcome_user(exercise)
+
+    for i in range(3):
+
+        progression, right_answer = make_progression()
+        print('Question:', progression)
+        answer = int(prompt.string('Your answer: '))
+
+        if make_check(name, answer, right_answer):
+            if i == 2:
+                print(f'Congratulations, {name}!')
+        else:
+            break
