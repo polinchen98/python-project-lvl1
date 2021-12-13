591a0b39dc5de4cc55dfeaa111bab08dcf758beb
Polina Rasskazova <rasskazova.pm@phystech.edu>
2021-12-13 12:39:26 +0300

    fixed linter's error

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -3,7 +3,7 @@ from random import randint
 
 def is_prime():
     a = randint(1, 1000)
-    for i in range(2, a-1):
+    for i in range(2, a - 1):
         if a % i == 0:
             return 'no', a
     return 'yes', a
--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -9,7 +9,7 @@ def make_progression():
     for i in range(x):
         a_1 = a_1 + d
         progression.append(a_1 + d)
-    a_n_index = randint(0, x-1)
+    a_n_index = randint(0, x - 1)
     missed_digit = progression[a_n_index]
     progression[a_n_index] = '..'
     progression = [str(x) for x in progression]
